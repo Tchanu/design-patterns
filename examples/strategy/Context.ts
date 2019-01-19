baf95e95deef5ce9b9c4ec32f55d065da0ede3be
tchanu <t.chanukvadze@gmail.com>
2019-01-19 18:47:16 +0400

    create context

---
+++ examples/strategy/Context.ts
@@ -0,0 +1,23 @@
+import Flying from './behaviour/Flying';
+import Talking from './behaviour/Talking';
+
+// in this example our context is Animal
+export class Animal {
+    // first we define behaviour/strategies
+    private flying: Flying;
+    private talking: Talking;
+
+    // client sets behaviours at runtime
+    constructor(flying: Flying, talking: Talking) {
+        this.flying = flying;
+        this.talking = talking;
+    }
+
+    doFly(): void {
+        this.flying.fly();
+    }
+
+    doTalk(): void {
+        this.talking.talk();
+    }
+}
