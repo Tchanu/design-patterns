746ccc9747269d43bd86d83d9c59012d395a32c2
tchanu <t.chanukvadze@gmail.com>
2019-01-19 18:46:59 +0400

    add behaviours

---
+++ examples/strategy/behaviour/Flying.ts
@@ -0,0 +1,15 @@
+export default interface Flying {
+    fly(): void;
+}
+
+export class FastFly implements Flying {
+    fly(): void {
+        console.log('fast fly');
+    }
+}
+
+export class NoFly implements Flying {
+    fly(): void {
+        console.log('can\'t fly');
+    }
+}
---
+++ examples/strategy/behaviour/Talking.ts
@@ -0,0 +1,15 @@
+export default interface Talking {
+    talk(): void;
+}
+
+export class SimpleTalk implements Talking {
+    talk(): void {
+        console.log('simple talk');
+    }
+}
+
+export class NoTalk implements Talking {
+    talk(): void {
+        console.log('can\'t talk');
+    }
+}
