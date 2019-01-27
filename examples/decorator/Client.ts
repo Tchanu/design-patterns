5efbc1639194cef22cea23e3193b4b6b20863eb4
tchanu <t.chanukvadze@gmail.com>
2019-01-27 17:53:39 +0400

    add decorator files

---
+++ examples/decorator/Client.ts
@@ -0,0 +1,23 @@
+import {MilkCoffee, VanillaCoffee} from './Decorator'
+import {SimpleCoffee} from "./Component";
+
+// simple milk
+const simpleCoffee = new SimpleCoffee();
+console.log(simpleCoffee.getCost()); // 5
+console.log(simpleCoffee.getDescription()); // Simple Coffee
+
+// coffee with milk
+const milkCoffee = new MilkCoffee(simpleCoffee);
+console.log(milkCoffee.getCost()); // 7
+console.log(milkCoffee.getDescription()); // Simple Coffee, milk
+
+
+// coffee with vanilla
+const vanillaCoffee = new VanillaCoffee(simpleCoffee);
+console.log(vanillaCoffee.getCost()); // 6
+console.log(vanillaCoffee.getDescription()); // Simple Coffee, vanilla
+
+// coffee with milk and vanilla
+const milkVanillaCoffee = new VanillaCoffee(milkCoffee);
+console.log(milkVanillaCoffee.getCost()); // 8
+console.log(milkVanillaCoffee.getDescription()); // Simple Coffee, milk, vanilla
---
+++ examples/decorator/Decorator.ts
@@ -0,0 +1,29 @@
+import {Coffee} from "./Component";
+
+// concrete decorator
+export class MilkCoffee implements Coffee {
+    constructor(protected coffee: Coffee) {
+    }
+
+    getCost(): number {
+        return this.coffee.getCost() + 2;
+    }
+
+    getDescription(): string {
+        return this.coffee.getDescription() + ", milk";
+    }
+}
+
+// concrete decorator 2
+export class VanillaCoffee implements Coffee {
+    constructor(protected coffee: Coffee) {
+    }
+
+    getCost(): number {
+        return this.coffee.getCost() + 1;
+    }
+
+    getDescription(): string {
+        return this.coffee.getDescription() + ", vanilla";
+    }
+}
